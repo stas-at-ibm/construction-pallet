83457cc1c7346a350f6ee9fee466212b12660096
Stanislav Jakuschevskij <stanislav.jakuschevskij@at.ibm.com>
2022-11-15 20:06:25 +0100

    added derive macro
    # added imports

--- pallets/template/src/types.rs
+++ pallets/template/src/types.rs
@@ -1,3 +1,8 @@
+use codec::{Decode, Encode};
+use frame_support::RuntimeDebug;
+use scale_info::TypeInfo;
+
+#[derive(Clone, Encode, Decode, Eq, PartialEq, RuntimeDebug, Default, TypeInfo)]
 pub struct Project<AccountId, BoundedString> {
 	pub id: BoundedString,
 	pub boq_id: BoundedString,
